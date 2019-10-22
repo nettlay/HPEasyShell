4891241c2fa5a3ae58e4d7b254f42259467226b9
nettl <yongli_cheng2008@163.com>
2019-10-22 16:58:52 +0800

    update elment lib for storefront

--- test.py
+++ test.py
@@ -1,24 +1,13 @@
-import platform
-import win32api, win32con
-import Test_Scripts.EasyShell_Lib as EasyshellLib
-import Library.CommonLib as CommonLib
-import os
-import time
-import traceback
-import pysnooper
-from PIL import ImageGrab, ImageDraw, ImageFont
-import subprocess
-import chardet
-import uiautomation
-import random
+from Test_Scripts import easyshell
 
 
-def logoff():
-    os.system('echo test_logon_pass >test_rdp_result.txt')
-    ftp = CommonLib.FTPUtils('15.83.251.201', 'sh\\cheng.balance', 'password.321')
-    ftp.change_dir('/Function/Automation/log/rdp')
-    ftp.upload_file('test_rdp_result.txt', 'test_rdp_result.txt')
-    ftp.close()
-    os.system('shutdown -l')
-
-CommonLib.WindowControl(Name="All Control Panel Items").Close()
+# def logoff():
+#     os.system('echo test_logon_pass >test_rdp_result.txt')
+#     ftp = CommonLib.FTPUtils('15.83.251.201', 'sh\\cheng.balance', 'password.321')
+#     ftp.change_dir('/Function/Automation/log/rdp')
+#     ftp.upload_file('test_rdp_result.txt', 'test_rdp_result.txt')
+#     ftp.close()
+#     os.system('shutdown -l')
+a = 'Naaae'
+test = None if a == 'None' else a
+print(test)
